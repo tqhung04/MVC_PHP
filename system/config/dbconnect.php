47718c1a6f1f691460da4338aeb2564d3a45b0c3
tqhung04 <tqhung04@gmail.com>
2017-03-08 10:06:00 +0700

    fix error font when export data from database

--- system/config/dbconnect.php
+++ system/config/dbconnect.php
@@ -4,6 +4,7 @@ class dbconnect {
 		// Connect Database
 		try {
 		    $conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . '', DB_USER, DB_PASSWORD);
+		    $conn->exec("set names utf8");
 		    return $conn;
 		} catch (PDOException $e) {
 		    print "Error!: " . $e->getMessage() . "<br/>";
